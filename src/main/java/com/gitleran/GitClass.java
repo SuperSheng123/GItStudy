3d7cd5163c3197296989aa6f839caabb7fda48a4
zhaosheng <1551956101@qq.com>
2022-02-27 10:47:53 +0800

    second commit

--- src/main/java/com/gitleran/GitClass.java
+++ src/main/java/com/gitleran/GitClass.java
@@ -3,5 +3,6 @@ package com.gitleran;
 public class GitClass {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
