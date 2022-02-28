77c0c4ed5de043000f1cd09838a97d58030fdc1e
zhaosheng <1551956101@qq.com>
2022-02-28 10:41:32 +0800

    push commit

--- src/main/java/com/gitleran/GitClass.java
+++ src/main/java/com/gitleran/GitClass.java
@@ -10,5 +10,6 @@ public class GitClass {
         System.out.println("hello git6");
         System.out.println("master-commit");
         System.out.println("hot-fix");
+        System.out.println("push-test");
     }
 }
